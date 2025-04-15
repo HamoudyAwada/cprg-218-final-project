9269c4d8f53ff1b0119bcc8228f0e6eef47e6327
HamoudyAwada <Mohamad.Awada04@outlook.com>
2025-04-15 03:02:58 -0600

    simplify header and begin styling in CSS

--- weather.js
+++ weather.js
@@ -5,11 +5,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=7519ed8775
     console.log(data.main.temp)
 
     temperature.textContent = "Temperature: " + data.main.temp + "\u00B0C";
-    description.textContent = "Description: " + data.weather[0].description;
-    humidity.textContent = "Humidity: " + data.main.humidity;
-    high.textContent = "High: " + data.main.temp_max;
-    low.textContent = "Low: " + data.main.temp_min;
-    realfeel.textContent = "Real Feel: " + data.main.feels_like + "\u00B0C";
+
 
     const weathericon = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + "@2x.png";
     console.log(weathericon);
